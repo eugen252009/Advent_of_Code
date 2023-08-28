939cffce17599311f3b3a6eb3ea797b2a3e1973d
Eugen <eugen252009@gmail.com>
2023-08-28 18:06:12 +0200

    edited package json to start projects with pnpm start  Changes to be committed: 	modified:   1/package.json 	modified:   2/package.json 	modified:   3/package.json 	modified:   4/index.ts 	modified:   4/package.json

--- 4/index.ts
+++ 4/index.ts
@@ -10,26 +10,27 @@ export const sample = `2-4,6-8
 2-8,3-7
 6-6,4-6
 2-6,4-8`
-
-// Preprocess Data for besser usage
-const data: string[] = input.split("\n")
-const tmp = data.map((item) => item.split(","))
-const tmp2 = tmp.map((item) =>
-	item
-		.map((items) => items.split("-").map((itemss) => parseInt(itemss)))
-		.flat(),
-)
+function preprocessData(inputstring: string) {
+	const data: string[] = inputstring.split("\n")
+	const tmp = data.map((item) => item.split(","))
+	return tmp.map((item) =>
+		item
+			.map((items) => items.split("-").map((itemss) => parseInt(itemss)))
+			.flat(),
+	)
+}
 
 // Process Data and store it in half1 or half 2
-
-tmp2.map((item, index) => {
-	if (checkoverlaphalf1([item[0], item[1]], [item[2], item[3]])) {
-		half1 += 1
-	}
-	if (checkoverlaphalf2([item[0], item[1]], [item[2], item[3]])) {
-		half2 += 1
-	}
-})
+function proccessData(data: number[][]) {
+	return data.map((item, index) => {
+		if (checkoverlaphalf1([item[0], item[1]], [item[2], item[3]])) {
+			half1 += 1
+		}
+		if (checkoverlaphalf2([item[0], item[1]], [item[2], item[3]])) {
+			half2 += 1
+		}
+	})
+}
 
 export function checkoverlaphalf1(number1: number[], number2: number[]) {
 	if (
@@ -52,3 +53,7 @@ export function checkoverlaphalf2(number1: number[], number2: number[]) {
 		return false
 	}
 }
+
+proccessData(preprocessData(sample))
+// proccessData(preprocessData(input))
+console.log(half1, half2)
