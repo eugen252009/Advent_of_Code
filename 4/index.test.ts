2890f07b179afdb12beb14bea67b724dd951f6f7
Eugen <eugen252009@gmail.com>
2023-08-28 17:40:46 +0200

    Changes to be committed: 	modified:   4/index.test.ts 	modified:   4/index.ts 	new file:   4/tsconfig.json

--- 4/index.test.ts
+++ 4/index.test.ts
@@ -1,8 +1,19 @@
 import { describe, expect, it } from "@jest/globals"
-import { checkoverlap, sample } from "."
+import { checkoverlaphalf2, checkoverlaphalf1 } from "."
 
 describe("index.ts", () => {
-	it("check for overlap", async () => {
-		expect(checkoverlap([2, 4], [6, 8])).toBe(false)
+	it("check for overlap half1", async () => {
+		expect(checkoverlaphalf1([2, 3], [4, 5])).toBe(false)
+		expect(checkoverlaphalf1([5, 7], [7, 9])).toBe(false)
+		expect(checkoverlaphalf1([2, 8], [3, 7])).toBe(true)
+		expect(checkoverlaphalf1([6, 6], [4, 6])).toBe(true)
+		expect(checkoverlaphalf1([2, 6], [4, 8])).toBe(false)
+	})
+	it("check for overlap half2", async () => {
+		expect(checkoverlaphalf2([2, 3], [4, 5])).toBe(false)
+		expect(checkoverlaphalf2([5, 7], [7, 9])).toBe(true)
+		expect(checkoverlaphalf2([2, 8], [3, 7])).toBe(true)
+		expect(checkoverlaphalf2([6, 6], [4, 6])).toBe(true)
+		expect(checkoverlaphalf2([2, 6], [4, 8])).toBe(true)
 	})
 })
--- 4/index.ts
+++ 4/index.ts
@@ -1,7 +1,9 @@
-// @ts-ignore
-import fs from "node:fs"
-// const input = fs.readFileSync("input.txt").toString()
+import fs from "fs"
+const input = fs.readFileSync("input.txt").toString()
 
+// number of elfes (Result)
+let half1 = 0
+let half2 = 0
 export const sample = `2-4,6-8
 2-3,4-5
 5-7,7-9
@@ -9,24 +11,38 @@ export const sample = `2-4,6-8
 6-6,4-6
 2-6,4-8`
 
-// every elf has assigned sections
-//
-//
-const data: string[] = sample.split("\n")
+// Preprocess Data for besser usage
+const data: string[] = input.split("\n")
 const tmp = data.map((item) => item.split(","))
 const tmp2 = tmp.map((item) =>
 	item
 		.map((items) => items.split("-").map((itemss) => parseInt(itemss)))
 		.flat(),
 )
-// console.log(tmp2)
-let id = 0
+
+// Process Data and store it in half1 or half 2
+
 tmp2.map((item, index) => {
-	if (checkoverlap([item[0], item[1]], [item[2], item[3]])) {
-		id += 1
+	if (checkoverlaphalf1([item[0], item[1]], [item[2], item[3]])) {
+		half1 += 1
+	}
+	if (checkoverlaphalf2([item[0], item[1]], [item[2], item[3]])) {
+		half2 += 1
 	}
 })
-export function checkoverlap(number1: number[], number2: number[]) {
+
+export function checkoverlaphalf1(number1: number[], number2: number[]) {
+	if (
+		(number1[0] <= number2[0] && number1[1] >= number2[1]) ||
+		(number2[0] <= number1[0] && number2[1] >= number1[1])
+	) {
+		return true
+	} else {
+		return false
+	}
+}
+
+export function checkoverlaphalf2(number1: number[], number2: number[]) {
 	if (
 		(number1[0] <= number2[1] && number1[1] >= number2[0]) ||
 		(number2[0] <= number1[1] && number2[1] >= number1[0])
@@ -36,4 +52,3 @@ export function checkoverlap(number1: number[], number2: number[]) {
 		return false
 	}
 }
-console.log(id)
