6fd992d5fc5a80a6b73cd3b85056b155f02bc1d5
Eugen <eugen252009@gmail.com>
2023-12-01 23:31:13 +0100

    Did Day one in Rust and javascript

---
+++ 2023/01/js/part1.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+let finalinput = fs.readFileSync("../input.txt").toString()
+let input = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
+
+function solve(inputstring) {
+	let counter = 0
+	const start = inputstring.split("\n")
+	for (const item of start) {
+		const items = item.match(/[0-9]/g)
+		if (items==undefined){break;}
+		const stringcount = items[0] + items[(items.length - 1)]
+		counter += parseInt(stringcount)
+	}
+	return counter
+}
+console.log(`Test input:\t ${solve(input)} should be 142`)
+console.log(`Final input:\t ${solve(finalinput)}`)
---
+++ 2023/01/rust/src/bin/part1.rs
@@ -0,0 +1,33 @@
+use regex::Regex;
+fn main() {
+    let input = include_str!("../../../input.txt");
+    let test_input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+    println!("Test Input:\t{},\tNeeds:{}", solve(test_input), 142);
+    println!("Final Input:\t{}", solve(input));
+}
+
+fn solve(inputstr: &str) -> i32 {
+    let lines: Vec<&str> = inputstr.split("\n").collect();
+    let number_regex = Regex::new(r"\d").unwrap();
+    let mut count = 0;
+    for line in lines {
+        let mut numbers = String::from("");
+        for number in number_regex.find_iter(line) {
+            numbers.push_str(number.as_str());
+        }
+        let len = numbers.len();
+
+        if let Some(first_char) = numbers.chars().nth(0) {
+            if let Some(last_char) = numbers.chars().nth(len - 1) {
+                if let Ok(number) = format!("{}{}", first_char, last_char).parse::<i32>() {
+                    count += number;
+                }
+                // println!("{}", count);
+            };
+        }
+    }
+    count
+}
