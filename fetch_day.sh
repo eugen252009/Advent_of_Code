62a59de627c66aa9a0e87d8324348150ac1e2659
Eugen <eugen252009@gmail.com>
2023-12-03 21:46:59 +0100

    added script for automaticly creating and downloading advent of Code inputs

--- fetch_day.sh
+++ fetch_day.sh
@@ -1,7 +1,18 @@
-if [ -d ./$1/day-$2]; then
+if [ -z "$1" ]; then 
+	year=$(date +%Y)
+else 
+	year=$1
+fi
+if [ -z "$2" ]; then 
+	today=$(date +%-d)
+else
+	today=$2
+fi
+
+if [ -d ./$year/day-$today]; then
 	echo "creating new Folder"
 else
-	mkdir ./$1/day-$2
-	echo "no Dir"
+	mkdir ./$year/day-$today
 fi
-curl https://adventofcode.com/$1/day/$2/input -c cookie > ./$1/day-$2/input
+
+curl https://adventofcode.com/$year/day/$today/input -b cookie > ./$year/day-$today/input
