e76f6d477adeb4af2c844a0a81cac1e16d776327
Eugen <eugen252009@gmail.com>
2024-05-05 18:08:42 +0200

    solved part1

--- 2023/day-05/js/part1.js
+++ 2023/day-05/js/part1.js
@@ -1,68 +1,119 @@
-import { readFileSync } from "fs"
-const input = readFileSync("../input", "utf8").slice(0, -1);
-
-const string = preprocessstring(input)
-solve(string)
-function solve(input) {
-	console.log(input.sort((a, b) => a - b)[0])
+const print = 0
+import { readFileSync } from "fs";
+function log(...a) {
+	if (print)
+		for (const item of a) {
+			console.log(item)
+		}
 }
+let input = `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`
+input = readFileSync("../input", "utf8").slice(0, -1);
+const string = preprocessstring(input)
+console.log(string.reduce((x, acc) => x < acc ? x : acc))
 function preprocessstring(input) {
-	const res = input.split("\n\n").map(line => line.split(":").map(line => line.replaceAll(" map", "").replaceAll("\n", " ").trim()))
-	const seed = res.shift()[1].split(" ")
-	const dict = createDict(res)
-	// console.log(dict.filter(dict=>dict.from=="light"))
+	const res = input.split("\n\n")
+		.map(line => line.split(":")
+			.map(line => line.replaceAll(" map", "")
+				.replaceAll("\n", " ")
+				.trim()))
+	const seed = res.shift()[1].split(" ");
+	const dict = createDict(res);
 	const data = []
 	for (const s of seed) {
 		data.push(walk(parseInt(s), dict, "seed"))
+		log("")
 	}
 	return data
 }
 function createDict(data) {
-	const dict = []
+	const dict = [];
 	for (const item of data) {
 		const [from, _, to] = item[0].split("-")
 		const path = parsePath(item[1])
-		// console.log(path)
 		dict.push({ from, to, path })
-	}
-	return dict
+	} return dict
 }
 
 function parsePath(int) {
-	const data = int.split(" ").map(num => parseInt(num))
-	// console.log(data,int)
-	const res = []
-	for (let index = 0; index <= data.length; index++) {
-		const [dest, source, range] = [data.shift(), data.shift(), data.shift()]
-		res.push([dest, source, range])
+	const path = []
+	const data = int.split(" ")
+	const tmp = []
+	for (let index = 0; index < data.length; index++) {
+		const element = data[index];
+		tmp.push(element)
 	}
-	const map = res.map(item => generateMap(item[0], item[1], item[2]))
-
-	const mergedMap = mergeMaps(map)
-	// console.log(mergedMap)
-	return mergedMap
-}
-function generateMap(dest, source, range) {
-	const map = {}
-	for (let i = 0; i < range; i++) {
-		map[source + i] = dest + i
+	for (let i = 0; i < tmp.length; i += 3) {
+		let [one, two, three] = tmp.slice(i, i + 3);
+		// log(
+		// 	{
+		// 		destinationmin: +one,
+		// 		destinationmax: (+one + +three - 1),
+		// 		sourcerangemin: +two,
+		// 		sourcerangemax: (+two + +three - 1),
+		// 	}
+		// )
+		path.push(
+			{
+				destmin: +one,
+				destmax: (+one + (+three)),
+				srcmin: +two,
+				srcmax: (+two + (+three)),
+			}
+		)
 	}
-	return map
-}
-function mergeMaps(maps) {
-	const map = {}
-	maps.map(element => {
-		Object.assign(map, element)
-	});
-	return map
+	return path
 }
 function walk(value, dict, name) {
-	// console.log({ value, name })
-	// if (name == undefined) return value
-	const dictionary = dict.find((item) => item.from === name)
-	if (dictionary?.to == undefined) return value
-	const newName = dictionary.to
-	const newVal = dictionary.path[value] ?? value
-	// console.log({ value, newval: newVal, newName })
-	return walk(newVal, dict, newName)
+	if (name == undefined) return value;
+	let dictionary = undefined
+	for (const item of dict) {
+		if (item.from === name) {
+			dictionary = item
+		}
+	}
+	if (dictionary == undefined) return value;
+	if (dictionary.to == undefined) return value;
+	const newName = dictionary.to;
+	for (let i = 0; i < dictionary.path.length; i++) {
+		log({ value, min: dictionary.path[i].srcmin, max: dictionary.path[i].srcmax });
+		if (dictionary.path[i].srcmin <= value && dictionary.path[i].srcmax > value) {
+			const newValue = ((value - dictionary.path[i].srcmin) + dictionary.path[i].destmin);
+			log({ name, value, newName, newValue });
+			return walk(newValue, dict, newName);
+		}
+	}
+	// log(`Nothing found ${value}`);
+	return walk(value, dict, newName);
 }
