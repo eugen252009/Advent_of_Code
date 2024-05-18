7ef83f0b6e07bb686f1de0f85a2f39d5a8dfda29
Eugen <eugen252009@gmail.com>
2024-05-18 16:52:19 +0200

    solved day-10 part1 and made a simple visualisation

--- 2023/day-10/js/part1.js
+++ 2023/day-10/js/part1.js
@@ -1,18 +1,15 @@
 import { readFileSync } from "fs"
-const input = readFileSync("../testinput", "utf8").split("\n").map(line => line.split("")).filter(item => item.length)
-
-const pipes = [
-	"|",
-	"-",
-	"J",
-	"7",
-	"L",
-	"F",
-]
+// let input = `..F7.
+// .FJ|.
+// SJ.L7
+// |F--J
+// LJ...`
+const input = readFileSync("../input", "utf8")
 const toBottom = [
 	"|",
 	"J",
-	"L"
+	"L",
+
 ]
 const toRight = [
 	"-",
@@ -25,107 +22,160 @@ const toTop = [
 	"F",
 ]
 const toLeft = [
-	"|",
-	"J",
+	"-",
+	"F",
 	"L"
 ]
+class Queue {
+	_queue = [];
+	constructor(data) {
+		if (data !== undefined)
+			for (const iter of data) {
+				this.enqueue(iter)
+			}
+	}
+	enqueue(item) {
+		this._queue.push(item)
+	}
+	get() {
+		return this._queue.shift()
+	}
+	length() {
+		return this._queue.length
+	}
+}
+const mazemap = parse(input, input.indexOf("\n"));
+// console.log(mazemap.map(x => x.middletile), input)
+const ways = walkMaze(mazemap)
+function walkMaze(maze) {
+	const que = new Queue();
+	const res = maze.reduce(
+		(acc, element, id) => {
+			if (element.mid.tile === "S") {
+				acc = id
+			};
+			return acc
+		}, -1)
+	que.enqueue(res)
+	const previouswalk = {}
+	const way = []
+	do {
+		const id = que.get()
+		const result = checkalldirs(maze[id])
+		if (result.up) {
+			const newid = maze[id].up.id
+			if (previouswalk[newid] == undefined) {
+				previouswalk[newid] = true
+				que.enqueue(newid)
+				way.push(newid)
+			}
+		}
+		if (result.down) {
+			const newid = maze[id].down.id
+			if (previouswalk[newid] == undefined) {
+				previouswalk[newid] = true
+				que.enqueue(newid)
+				way.push(newid)
+			}
+		}
+		if (result.right) {
+			const newid = maze[id].right.id
+			if (previouswalk[newid] == undefined) {
+				previouswalk[newid] = true
+				que.enqueue(newid)
+				way.push(newid)
+			}
+		}
+		if (result.left) {
+			const newid = maze[id].left.id
+			if (previouswalk[newid] == undefined) {
+				previouswalk[newid] = true
+				que.enqueue(newid)
+				way.push(newid)
+			}
+		}
+	} while (que.length() !== 0);
+	return way
+}
+function checkalldirs(tile) {
+	switch (tile.mid.tile) {
+		case "-": {
+			return {
+				right: tile.right == undefined ? undefined : checkright(tile),
+				left: tile.up == undefined ? undefined : checkleft(tile),
+			}
+		}
+		case "|": {
+			return {
+				up: tile.up == undefined ? undefined : checkup(tile),
+				down: tile.down == undefined ? undefined : checkdown(tile),
+			}
+		}
+		case "7":
+			return {
+				down: tile.down == undefined ? undefined : checkdown(tile),
+				left: tile.left == undefined ? undefined : checkleft(tile),
+			}
+		case "L":
+			return {
+				up: tile.up == undefined ? undefined : checkup(tile),
+				right: tile.right == undefined ? undefined : checkright(tile),
+			}
+		case "F":
+			return {
+				down: tile.down == undefined ? undefined : checkdown(tile),
+				right: tile.right == undefined ? undefined : checkright(tile),
+			}
+		case "J":
+			return {
+				up: tile.up == undefined ? undefined : checkup(tile),
+				left: tile.left == undefined ? undefined : checkleft(tile),
+			}
 
-const [result, data] = solve(input)
-// console.log(result, "\n", data)
-console.log(data)
-function solve(input) {
-	let [x, y] = findStart(input)
-	input[x][y] = "0"
-	let steps = 1
-	while (true) {
-		const [top, bottom, right, left] = findpipes({ input, x, y })
-		if (toRight.includes(right[2]) !== -1) {
-			x = right[0]
-			y = right[1]
-			console.log(x, y)
-			input[x][y] = steps++
-			continue
-		}
-		if (toLeft.includes(left[2]) !== -1) {
-			x = left[0]
-			y = left[1]
-			console.log(x, y)
-			input[x][y] = steps++
-			continue
-		}
-		if (toTop.includes(top[2]) !== -1) {
-			x = top[0]
-			y = top[1]
-			console.log(x, y)
-			input[x][y] = steps++
-			continue
-		}
-		if (toBottom.includes(bottom[2]) !== -1) {
-			x = bottom[0]
-			y = bottom[1]
-			console.log(x, y)
-			input[x][y] = steps++
-			continue
-		}
-		break
 
-		// if (bottom[2] !== -1 && toBottom.includes(pipes[bottom[2]])) {
-		// 	// console.log(pipes[bottom[2]])
-		// 	x = bottom[0]
-		// 	y = bottom[1]
-		// 	input[bottom[0]][bottom[1]] = steps++
-		// 	continue
-		// }
-		// if (right[2] !== -1 && toRight.includes(pipes[right[2]])) {
-		// 	// console.log(pipes[bottom[2]])
-		// 	x = right[0]
-		// 	y = right[1]
-		// 	input[x][y] = steps++
-		// 	continue
-		// }
-		// if (top[2] !== -1 && toTopludes(pipes[top[2]])) {
-		// 	console.log(pipes[top[2]])
-		// 	x = top[0]
-		// 	y = top[1]
-		// 	input[x][y] = steps++
-		// 	continue
-		// }
-		// if (left[2] !== -1 && toRight.includes(pipes[right[2]])) {
-		// 	// console.log(pipes[bottom[2]])
-		// 	x = right[0]
-		// 	y = right[1]
-		// 	input[x][y] = steps++
-		// 	continue
-		// }
-		// break
-		//
-	}
-	console.log({ steps }, input)
-	return [steps / 2, input]
-}
-function getpipeid({ input, x, y }) {
-	return {
-		left: pipes.indexOf(input[x][y - 1]),
-		right: pipes.indexOf(input[x][y + 1]),
-		top: pipes.indexOf(input[x - 1][y]),
-		bottom: pipes.indexOf(input[x + 1][y]),
+
+		default:
+			return {
+				up: tile.up == undefined ? undefined : checkup(tile),
+				right: tile.right == undefined ? undefined : checkright(tile),
+				left: tile.up == undefined ? undefined : checkup(tile),
+				down: tile.up == undefined ? undefined : checkup(tile),
+			}
 	}
 }
-function findStart(string) {
-	let x, y;
-	for (let i = 0; i < string.length; i++) {
-		if (string[i].indexOf("S") !== -1) {
-			x = i
-			y = string[i].indexOf("S")
-		}
-	}
-	return [x, y]
+function checkright(tile) {
+	return toRight.includes(tile.right.tile) ? true : false;
 }
-function findpipes({ input, x, y }) {
-	return [
-		[x - 1, y, pipes.indexOf(input[x - 1][y])],
-		[x + 1, y, pipes.indexOf(input[x + 1][y])],
-		[x, y + 1, pipes.indexOf(input[x][y + 1])],
-		[x, y - 1, pipes.indexOf(input[x][y - 1])],
-	]
+function checkleft(tile) {
+	return toLeft.includes(tile.left.tile) ? true : false;
 }
+function checkup(tile) {
+	return toTop.includes(tile.up.tile) ? true : false;
+}
+function checkdown(tile) {
+	return toBottom.includes(tile.down.tile) ? true : false;
+}
+
+
+function parse(maze, cols) {
+	const [vertical, horizontal] = [cols, 1];
+	const mazemap = [];
+	maze.split("").filter(x => x !== "\n")
+		.forEach((element, index, arr) => {
+			const middle = element
+			const up = arr[index - vertical] != undefined ? { id: index - vertical, tile: arr[index - vertical] } : undefined
+			const down = arr[index + vertical] != undefined ? { id: index + vertical, tile: arr[index + vertical] } : undefined
+			const right = arr[index + horizontal] != undefined ? { id: index + horizontal, tile: arr[index + horizontal] } : undefined
+			const left = arr[index - horizontal] != undefined ? { id: index - horizontal, tile: arr[index - horizontal] } : undefined
+			mazemap.push(
+				{
+					mid: { id: index, tile: middle },
+					up,
+					down,
+					left,
+					right,
+				})
+		});
+	return mazemap
+}
+console.log((ways.length + 1) / 2)
--- 2023/day-10/js/part2.js
+++ 2023/day-10/js/part2.js
@@ -1,42 +1,175 @@
-Array.prototype.last = function() {
-	return this[this.length - 1]
-}
-Array.prototype.first = function() {
-	return this[0]
-}
-
 import { readFileSync } from "fs"
 const input = readFileSync("../input", "utf8")
-	.split("\n")
-	.filter(line => line.length)
-	.map(i => i.split(" ").flatMap(i => parseInt(i)))
-solve(input)
-function solve(input) {
-	const res = [];
-	for (const data of input) {
-		res.push(calc(data))
+const toBottom = [
+	"|",
+	"J",
+	"L",
+]
+const toRight = [
+	"-",
+	"J",
+	"7",
+]
+const toTop = [
+	"|",
+	"7",
+	"F",
+]
+const toLeft = [
+	"-",
+	"F",
+	"L"
+]
+class Queue {
+	_queue = [];
+	constructor(data) {
+		if (data !== undefined)
+			for (const iter of data) {
+				this.enqueue(iter)
+			}
 	}
-	const result = res.reduce((acc, val) => acc + val, 0)
-	console.log("result:\t", result)
+	enqueue(item) {
+		this._queue.push(item)
+	}
+	get() {
+		return this._queue.shift()
+	}
+	length() {
+		return this._queue.length
+	}
 }
-function calc(slice) {
-	const res = [slice]
-	for (let i = 0; i < res.length; i++) {
-		const data = []
-		for (let j = 0; j < res[i].length - 1; j++) {
-			const element = res[i][j]
-			const element2 = res[i][j + 1]
-			data.push(element2 - element)
+const mazemap = parse(input, input.indexOf("\n"));
+// console.log(mazemap.map(x => x.middletile), input)
+const ways = walkMaze(mazemap)
+function walkMaze(maze) {
+	const que = new Queue();
+	const res = maze.reduce(
+		(acc, element, id) => {
+			if (element.mid.tile === "S") {
+				acc = id
+			};
+			return acc
+		}, -1)
+	que.enqueue(res)
+	const previouswalk = {}
+	const way = []
+	do {
+		const id = que.get()
+		const result = checkalldirs(maze[id])
+		if (result.up) {
+			const newid = maze[id].up.id
+			if (previouswalk[newid] == undefined) {
+				previouswalk[newid] = true
+				que.enqueue(newid)
+				way.push(newid)
+			}
 		}
-		res.push(data)
-		if (data.reduce((acc, val) => acc - val) == 0) { break }
-	}
-	console.log(res)
-	for (let i = res.length - 1; i > 0; i--) {
-		if (i - 1 >= 0)
-		res[i - 1].unshift(res[i-1][0] - res[i][0])
-		console.log({ two: res[i][0], one: res[i - 1][0] })
-	}
-	console.log(res)
-	return res.first().first()
+		if (result.down) {
+			const newid = maze[id].down.id
+			if (previouswalk[newid] == undefined) {
+				previouswalk[newid] = true
+				que.enqueue(newid)
+				way.push(newid)
+			}
+		}
+		if (result.right) {
+			const newid = maze[id].right.id
+			if (previouswalk[newid] == undefined) {
+				previouswalk[newid] = true
+				que.enqueue(newid)
+				way.push(newid)
+			}
+		}
+		if (result.left) {
+			const newid = maze[id].left.id
+			if (previouswalk[newid] == undefined) {
+				previouswalk[newid] = true
+				que.enqueue(newid)
+				way.push(newid)
+			}
+		}
+	} while (que.length() !== 0);
+	return way
 }
+function checkalldirs(tile) {
+	switch (tile.mid.tile) {
+		case "-": {
+			return {
+				right: tile.right == undefined ? undefined : checkright(tile),
+				left: tile.up == undefined ? undefined : checkleft(tile),
+			}
+		}
+		case "|": {
+			return {
+				up: tile.up == undefined ? undefined : checkup(tile),
+				down: tile.down == undefined ? undefined : checkdown(tile),
+			}
+		}
+		case "7":
+			return {
+				down: tile.down == undefined ? undefined : checkdown(tile),
+				left: tile.left == undefined ? undefined : checkleft(tile),
+			}
+		case "L":
+			return {
+				up: tile.up == undefined ? undefined : checkup(tile),
+				right: tile.right == undefined ? undefined : checkright(tile),
+			}
+		case "F":
+			return {
+				down: tile.down == undefined ? undefined : checkdown(tile),
+				right: tile.right == undefined ? undefined : checkright(tile),
+			}
+		case "J":
+			return {
+				up: tile.up == undefined ? undefined : checkup(tile),
+				left: tile.left == undefined ? undefined : checkleft(tile),
+			}
+
+
+
+		default:
+			return {
+				up: tile.up == undefined ? undefined : checkup(tile),
+				right: tile.right == undefined ? undefined : checkright(tile),
+				left: tile.up == undefined ? undefined : checkup(tile),
+				down: tile.up == undefined ? undefined : checkup(tile),
+			}
+	}
+}
+function checkright(tile) {
+	return toRight.includes(tile.right.tile) ? true : false;
+}
+function checkleft(tile) {
+	return toLeft.includes(tile.left.tile) ? true : false;
+}
+function checkup(tile) {
+	return toTop.includes(tile.up.tile) ? true : false;
+}
+function checkdown(tile) {
+	return toBottom.includes(tile.down.tile) ? true : false;
+}
+
+
+function parse(maze, cols) {
+	const [vertical, horizontal] = [cols, 1];
+	const mazemap = [];
+	maze.split("").filter(x => x !== "\n")
+		.forEach((element, index, arr) => {
+			const middle = element
+			const up = arr[index - vertical] != undefined ? { id: index - vertical, tile: arr[index - vertical] } : undefined
+			const down = arr[index + vertical] != undefined ? { id: index + vertical, tile: arr[index + vertical] } : undefined
+			const right = arr[index + horizontal] != undefined ? { id: index + horizontal, tile: arr[index + horizontal] } : undefined
+			const left = arr[index - horizontal] != undefined ? { id: index - horizontal, tile: arr[index - horizontal] } : undefined
+			mazemap.push(
+				{
+					mid: { id: index, tile: middle },
+					up,
+					down,
+					left,
+					right,
+				})
+		});
+	return mazemap
+}
+console.log((ways.length + 1) / 2)
