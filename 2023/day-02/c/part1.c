7e780bef7b572bbb192089d45e6429b999148125
Eugen <eugen252009@gmail.com>
2024-08-18 22:16:26 +0200

    read a line and find the first game

--- 2023/day-02/c/part1.c
+++ 2023/day-02/c/part1.c
@@ -2,7 +2,6 @@
 #include <fcntl.h>
 #include <locale.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #define BUFSIZE 4090
@@ -14,39 +13,23 @@ void handleError(int errcode) {
     _exit(1);
   }
 }
-char **SplitNewLine(char *text, size_t size) {
-  char **ptr = malloc(size);
-  int last = 0;
-  int id = 0;
-  for (int i = 0; i < size; i++) {
-    if (text[i] == '\n') {
-      last = i;
-      char tmp[BUFSIZE] = {};
-      char *ptr2 = strncpy(tmp, text, i);
-      ptr[id++] = tmp;
-    }
-  }
-  return ptr;
-}
 
 int main(int argc, char **argv) {
-  char *filepath = "../textinput.txt";
+  char *filepath = "../testinput.txt";
   if (argc > 1) {
     filepath = argv[1];
   }
-  int filestream = open(filepath, O_RDONLY);
-  handleError(errno);
+  FILE *filestream = fopen(filepath, "r");
+  if (filestream == NULL) {
+    printf("%s", "Could not Open File!");
+    // _exit(1);
+    return 1;
+  }
   char buf[BUFSIZE] = {};
-  char carry[BUFSIZE] = {};
-  size_t size = read(filestream, buf, BUFSIZE);
-
-  while (size != 0) {
-    char **ptr2 = SplitNewLine(buf, size);
-    for (int i = 0; i < size; i++) {
-      printf("%s", ptr2[i]);
-    }
-    free(ptr2);
-    size = read(filestream, buf, 1024);
+  while (fgets(buf, BUFSIZE, filestream)) {
+    char *begin = strchr(buf, ':');
+    char *ptr = strchr(buf, ';');
+    printf("Beginn: %zu, abschluss erste Runde: %zu\n", begin - buf, ptr - buf);
   }
   return 0;
 }
